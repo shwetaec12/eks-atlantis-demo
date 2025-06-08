bac96b044219d40be8fe843b67dfe392c680cbfc
Avinash Roy <nashavi12@gmail.com>
2025-06-08 15:14:29 -0700

    test

--- environment/test/providers.tf
+++ environment/test/providers.tf
@@ -3,7 +3,28 @@ provider "aws" {
   profile = var.aws_profile
 }
 
+# Get EKS cluster details after creation
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
+
+# Get authentication token to access the cluster
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+# Kubernetes provider using EKS details
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+# Helm provider using Kubernetes config
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
 }
